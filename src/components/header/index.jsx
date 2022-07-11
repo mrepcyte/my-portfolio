670b78506532e7cee53dc175937153a4f42c6d52
mrepcyte <monika.rep@gmail.com>
2022-07-11 21:38:25 +0300

    additional link

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,18 @@
-import React from "react";
-import { HeaderComponent, NAV, A } from "./styles";
+import React from 'react';
+import { HeaderComponent, NAV, A } from './styles';
 
 const Header = () => {
     return (
         <HeaderComponent>
             <NAV>
-                <A href="mailto: monika.rep@gmail.com" target="_blank">
+                <A href='mailto: monika.rep@gmail.com' target='_blank'>
                     mail
                 </A>
-                <A href="https://github.com/mrepcyte" target="_blank">
+                <A href='https://github.com/mrepcyte' target='_blank'>
                     github
+                </A>
+                <A href='https://www.frontendmentor.io/profile/mrepcyte' target='_blank'>
+                    frontend mentor
                 </A>
             </NAV>
         </HeaderComponent>
